9eef2ceb4fcfce0c82b53bae04cd340e516c464d
Peter Sutcliffe <sutcliffe_peter@hotmail.com>
2013-05-14 14:02:48 +0100

    Reworked UI code to be more like book

--- AuctionSniper/AuctionSniper.UI.Wpf/App.xaml.cs
+++ AuctionSniper/AuctionSniper.UI.Wpf/App.xaml.cs
@@ -1,21 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
+using AuctionSniper.XMPP;
 
 namespace AuctionSniper.UI.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            SniperWindow.Start(e.Args);
+            AppMain.Run(e.Args);
+        }
+    }
+
+    public class AppMain : IAuctionEventListener
+    {
+        private readonly SniperWindow _ui;
+
+        private const int ArgHostName = 0;
+        private const int ArgUserName = 1;
+        private const int ArgItemId = 2;
+        private const string ItemIdAsLogin = "auction-{0}";        
+
+        public static void Run(string[] args)
+        {            
+            var appMain = new AppMain();
+
+            var hostName = args[ArgHostName];
+            var userName = args[ArgUserName];
+            var itemId = args[ArgItemId];
+
+            appMain.JoinAuction(hostName, userName, itemId);
+        }
+
+        public AppMain()
+        {
+            _ui = new SniperWindow();
+            _ui.Show();
+        }
+
+        public void AuctionClosed()
+        {
+            _ui.ShowStatus(ApplicationConstants.StatusLost);
+        }
+
+        public void CurrentPrice(int price, int increment)
+        {
+
+        }
+
+        private static Connection ConnectTo(string hostName, string userName)
+        {
+            var connection = new Connection(hostName, userName);
+            connection.Connect();
+            return connection;
+        }
+
+        private static string AuctionId(string itemId)
+        {
+            return string.Format(ItemIdAsLogin, itemId);
+        }
+
+        private void JoinAuction(string hostName, string userName, string itemId)
+        {
+            var connection = ConnectTo(hostName, userName);
+
+            var chat = connection.GetChatManager().CreateChat(AuctionId(itemId), new AuctionMessageTranslator(this).ProcessMessage);
+            chat.SendMessage(new Message(ApplicationConstants.JoinCommandFormat));
         }
     }
 }
--- AuctionSniper/AuctionSniper.UI.Wpf/SniperWindow.xaml.cs
+++ AuctionSniper/AuctionSniper.UI.Wpf/SniperWindow.xaml.cs
@@ -3,64 +3,17 @@ using AuctionSniper.XMPP;
 
 namespace AuctionSniper.UI.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class SniperWindow : IAuctionEventListener
-    {
-        private const int ArgHostName = 0;
-        private const int ArgUserName = 1;
-        private const int ArgItemId = 2;
-
-        private const string ItemIdAsLogin = "auction-{0}";
-
-        public static void Start(string[] args)
-        {
-            var window = new SniperWindow();
-            window.Show();
-            var hostName = args[ArgHostName];
-            var userName = args[ArgUserName];
-            var itemId = args[ArgItemId];
-
-            window.JoinAuction(hostName, userName, itemId);
-        }
-
-        private void JoinAuction(string hostName, string userName, string itemId)
-        {
-            var connection = ConnectTo(hostName, userName);
-
-            var chat = connection.GetChatManager().CreateChat(
-                AuctionId(itemId), new AuctionMessageTranslator(this).ProcessMessage
-                );
-            chat.SendMessage(new Message(ApplicationConstants.JoinCommandFormat));
-        }
-
-        public void AuctionClosed()
-        {
-            Dispatcher.BeginInvoke((Action)delegate { SniperStatus.Text = ApplicationConstants.StatusLost; });
-        }
-
-        public void CurrentPrice(int price, int increment)
-        {
-            
-        }
-
-        private static Connection ConnectTo(string hostName, string userName)
-        {
-            var connection = new Connection(hostName, userName);
-            connection.Connect();
-            return connection;
-        }
-
-        private static string AuctionId(string itemId)
-        {
-            return string.Format(ItemIdAsLogin, itemId);
-        }
-
-        private SniperWindow()
+    public partial class SniperWindow
+    {        
+        public SniperWindow()
         {
             InitializeComponent();
             SniperStatus.Text = ApplicationConstants.StatusJoining;
+        }
+
+        public void ShowStatus(string status)
+        {
+            Dispatcher.BeginInvoke((Action)delegate { SniperStatus.Text = status; });
         }
     }
 }
